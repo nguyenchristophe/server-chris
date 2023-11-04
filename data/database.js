89ebe251e1d991239b927a3a8bf8b5356774f6d6
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-11-04 18:29:35 +0100

    Update database.js

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "UdemyCoursetest",
+      dbName: "AppliverseZen",
     });
 
     console.log(`Server connected to database ${connection.host}`);
