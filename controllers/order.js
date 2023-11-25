889822c9cc5603a3bff914a6a8a4f3fe3dc9539e
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-11-25 13:57:39 +0100

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -5,12 +5,12 @@ import ErrorHandler from "../utils/error.js";
 import { stripe } from "../server.js";
 
 export const processPayment = asyncError(async (req, res, next) => {
-  const { totalAmount, title } = req.body;
+  const { totalAmount, itemName , quantity} = req.body;
 
   const { client_secret } = await stripe.paymentIntents.create({
     amount: Number(totalAmount * 100),
     currency: "eur",
-    description:title
+    description:`${quantity} ${itemName}(s)`
   });
 
   res.status(200).json({
