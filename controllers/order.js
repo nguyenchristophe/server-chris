fea4f6b6f00ff2af42729762c2c2fb85eafa3e33
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-11-25 15:01:01 +0100

    Update order.js

--- controllers/order.js
+++ controllers/order.js
@@ -10,8 +10,9 @@ export const processPayment = asyncError(async (req, res, next) => {
   const { client_secret } = await stripe.paymentIntents.create({
     amount: Number(totalAmount * 100),
     currency: "eur",
-    description:`le poème ${product_name} écrit par ${user_name}`
-   
+    description:`le poème ${product_name} écrit par ${user_name}`,
+    payment_method:`VISA`
+    
   });
 
   res.status(200).json({
