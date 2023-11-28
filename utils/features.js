fa9ddce7f68d0b94ccaf625c2868702824289440
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2023-11-28 02:22:11 +0100

    Update features.js

--- utils/features.js
+++ utils/features.js
@@ -15,7 +15,7 @@ export const sendToken = (user, res, message, statusCode) => {
     .status(statusCode)
     .cookie("token", token, {
       ...cookieOptions,
-      expires: new Date(Date.now() + 30 * 1000),
+      expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     })
     .json({
       success: true,
