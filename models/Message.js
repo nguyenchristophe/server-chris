9bbd6c581cea008c3aa577a2df76dbd507942831
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2024-01-29 00:33:13 +0100

    Create Message.js

---
+++ models/Message.js
@@ -0,0 +1,21 @@
+
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Référence au modèle d'utilisateur que vous avez déjà
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
