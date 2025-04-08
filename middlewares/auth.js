5a6f34e1acae106ea6580fc660deab1087097114
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-04-08 16:29:01 +0200

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,6 +16,32 @@ export const isAuthenticated = asyncError(async (req, res, next) => {
 
   next();
 });
+export const isOwnerOrAdmin = async (req, res, next) => {
+  const user = req.user; // l'utilisateur connecté
+  const productId = req.params.id;
+
+  // on retrouve le product
+  const product = await Product.findById(productId);
+  if (!product) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Poème introuvable" });
+  }
+
+  // Si admin -> OK
+  if (user.role === "admin") return next();
+
+  // Sinon, si l'utilisateur est owner du product -> OK
+  if (product.owner.toString() === user._id.toString()) {
+    return next();
+  }
+
+  // Sinon -> 403
+  return res.status(403).json({
+    success: false,
+    message: "Accès refusé : vous n'êtes ni administrateur, ni propriétaire.",
+  });
+};
 
 export const isAdmin = asyncError(async (req, res, next) => {
   if (req.user.role !== "admin")
