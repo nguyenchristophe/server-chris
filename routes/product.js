bc5ffd7d44014e71d46edf74d04f74435e5caaf3
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-04-08 17:33:17 +0200

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -13,11 +13,11 @@ import {
   updateProduct,
   rentPoem
 } from "../controllers/product.js";
-import { isAuthenticated, isPoetOrAdmin  } from "../middlewares/auth.js";
+import { isAuthenticated, isPoetOrAdmin, isOwnerOrAdmin } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
 import { voteProduct, likeProduct } from "../controllers/product.js";
 
-import { addAssetToProduct } from "../controllers/product.js";
+import { addAssetToProduct, getMyPoems } from "../controllers/product.js";
 
 
 
