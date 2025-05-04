a0759096428c9d6497b89df65b8d6e5070a14706
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-05-04 13:16:38 +0200

    Create playlist.js

---
+++ routes/playlist.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { isAuthenticated } from "../middlewares/auth.js";
+import {
+  createPlaylist,
+  getMyPlaylists,
+  addPoemToPlaylist,
+  removePoemFromPlaylist,
+  incrementView,
+} from "../controllers/playlistController.js";
+
+const router = express.Router();
+
+router.post("/", isAuthenticated, createPlaylist);
+router.get("/me", isAuthenticated, getMyPlaylists);
+router.put("/:id/add", isAuthenticated, addPoemToPlaylist);
+router.put("/:id/remove", isAuthenticated, removePoemFromPlaylist);
+router.post("/:playlistId/poem/:poemId/view", incrementView);
+
+export default router;
