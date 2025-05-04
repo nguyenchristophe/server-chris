2aa453bacf02408527ff6189ec897b8fbb202b12
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-05-04 13:15:26 +0200

    Update playlist.js

--- models/playlist.js
+++ models/playlist.js
@@ -1,1 +1,17 @@
 
+// models/playlist.js
+import mongoose from "mongoose";
+
+const playlistSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  poems: [
+    {
+      poem: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      views: { type: Number, default: 0 }
+    }
+  ],
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
