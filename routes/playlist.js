f46091a460b1dd040e7f11722805639e8b24bb8c
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-06-02 18:27:25 +0200

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -10,7 +10,7 @@ import {
 
 const router = express.Router();
 
-router.post("/", isAuthenticated, createPlaylist);
+router.post("/create", isAuthenticated, createPlaylist);
 
 
 router.get("/me", isAuthenticated, getMyPlaylists);
