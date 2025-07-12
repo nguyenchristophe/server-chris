c6973eb2003aaeaf98f958bffecdbf1c2c26a827
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-07-12 16:44:24 +0200

    Update mintLicence.js

--- routes/mintLicence.js
+++ routes/mintLicence.js
@@ -1,7 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const { mintLicense } = require("../controllers/fanLicenseController");
 
-router.post("/mint-license", mintLicense);
-
-module.exports = router;
