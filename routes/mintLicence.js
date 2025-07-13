40c0014e9dafbf2d139acba7fa3b2fb250122ccc
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-07-13 10:49:03 +0200

    Update mintLicence.js

--- routes/mintLicence.js
+++ routes/mintLicence.js
@@ -3,6 +3,6 @@ import { mintLicense } from "../controllers/fanLicenceController.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.post("/mint-license",isAuthenticated,mintLicense);
+router.post("/mintlicense",isAuthenticated,mintLicense);
 
 export default router;
