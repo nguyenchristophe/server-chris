81d3686d1f21e9a680b3a4a43824509d5d6f7802
nguyenchristophe <34864984+nguyenchristophe@users.noreply.github.com>
2025-08-16 22:07:28 +0200

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
+
 import {
   createPlaylist,
   getMyPlaylists,
   addPoemToPlaylist,
   removePoemFromPlaylist,
   incrementView,
+  updatePlaylistOrder,   // +++
+  updatePlaylistMeta,    // +++
 } from "../controllers/playlistController.js";
+
+
 
 const router = express.Router();
 
@@ -17,5 +22,8 @@ router.get("/me", isAuthenticated, getMyPlaylists);
 router.put("/:id/add", isAuthenticated, addPoemToPlaylist);
 router.put("/:id/remove", isAuthenticated, removePoemFromPlaylist);
 router.post("/:playlistId/poem/:poemId/view", incrementView);
+
+router.put("/:id/order", isAuthenticated, updatePlaylistOrder);
+router.put("/:id/meta", isAuthenticated, updatePlaylistMeta);
 
 export default router;
